7561c21874a75f9cd71dd65710f007276dd0331e
iamparves <parves.pe.wb.h@gmail.com>
2024-05-27 17:44:01 +0600

    doctor routes added

--- src/app/controllers/doctor.controller.js
+++ src/app/controllers/doctor.controller.js
@@ -1,3 +1,4 @@
+import AppError from "../../utils/appError.js";
 import catchAsync from "../../utils/catchAsync.js";
 import filterObj from "../../utils/filterObj.js";
 import Doctor from "../models/doctor.model.js";
@@ -36,5 +37,112 @@ export const createDoctor = catchAsync(async (req, res, next) => {
     data: {
       doctor,
     },
+  });
+});
+
+export const getAllDoctors = catchAsync(async (req, res, next) => {
+  const doctors = await Doctor.find();
+
+  return res.status(200).json({
+    status: "success",
+    message: "Doctors found successfully",
+    results: doctors.length,
+    data: {
+      doctors,
+    },
+  });
+});
+
+export const getDoctorById = catchAsync(async (req, res, next) => {
+  const doctor = await Doctor.findById(req.params.doctorId).populate(
+    "hospital"
+  );
+
+  if (!doctor) {
+    return next(new AppError("Doctor not found", 404));
+  }
+
+  return res.status(200).json({
+    status: "success",
+    message: "Doctor found successfully",
+    data: {
+      doctor,
+    },
+  });
+});
+
+export const updateDoctor = catchAsync(async (req, res, next) => {
+  const doctorData = filterObj(
+    req.body,
+    "name",
+    "photo",
+    "qualifications",
+    "about",
+    "specialities",
+    "designation",
+    "languages",
+    "institute",
+    "department",
+    "appointmentNo",
+    "chamberTime",
+    "offDays",
+    "floorNo",
+    "roomNumber",
+    "branchNames",
+    "bmdcNo",
+    "consulatationFee",
+    "phone",
+    "feesToShowReport"
+  );
+
+  const doctor = await Doctor.findById(req.params.doctorId);
+
+  if (!doctor) {
+    return next(new AppError("Doctor not found", 404));
+  }
+
+  if (doctor.hospital.toString() !== req.user.profile.toString()) {
+    return next(
+      new AppError("You are not authorized to update this doctor", 403)
+    );
+  }
+
+  const updatedDoctor = await Doctor.findByIdAndUpdate(
+    req.params.doctorId,
+    doctorData,
+    {
+      new: true,
+      runValidators: true,
+    }
+  );
+
+  return res.status(200).json({
+    status: "success",
+    message: "Doctor updated successfully",
+    data: {
+      doctor: updatedDoctor,
+    },
+  });
+});
+
+export const deleteDoctor = catchAsync(async (req, res, next) => {
+  const doctor = await Doctor.findById(req.params.doctorId);
+
+  if (!doctor) {
+    return next(new AppError("Doctor not found", 404));
+  }
+
+  if (doctor.hospital.toString() !== req.user.profile.toString()) {
+    return next(
+      new AppError("You are not authorized to delete this doctor", 403)
+    );
+  }
+
+  await Doctor.findByIdAndDelete(req.params.doctorId);
+
+  return res.status(204).json({
+    status: "success",
+    message: "Doctor deleted successfully",
+    data: null,
   });
 });
--- src/app/routes/doctor.route.js
+++ src/app/routes/doctor.route.js
@@ -1,9 +1,24 @@
 import express from "express";
 import { protect, restrictTo } from "../controllers/auth.controller.js";
-import { createDoctor } from "../controllers/doctor.controller.js";
+import {
+  createDoctor,
+  deleteDoctor,
+  getAllDoctors,
+  getDoctorById,
+  updateDoctor,
+} from "../controllers/doctor.controller.js";
 
 const doctorRouter = express.Router();
 
-doctorRouter.post("/", protect, restrictTo("hospital"), createDoctor);
+doctorRouter
+  .route("/")
+  .get(getAllDoctors)
+  .post(protect, restrictTo("hospital"), createDoctor);
+
+doctorRouter
+  .route("/:doctorId")
+  .get(getDoctorById)
+  .patch(protect, restrictTo("hospital"), updateDoctor)
+  .delete(protect, restrictTo("hospital"), deleteDoctor);
 
 export default doctorRouter;
