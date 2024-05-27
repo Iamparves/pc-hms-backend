8662beb3d555799f4c644b04656e3c258f28fce4
iamparves <parves.pe.wb.h@gmail.com>
2024-05-27 15:06:11 +0600

    fix user controller

--- src/app/controllers/user.controller.js
+++ src/app/controllers/user.controller.js
@@ -2,14 +2,10 @@ import catchAsync from "../../utils/catchAsync.js";
 import User from "../models/user.model.js";
 
 export const getMe = catchAsync(async (req, res, next) => {
-  const user = await User.findById(req.user._id)
-    .select(
-      "-password -verificationOTP -verificationOTPExpires -resetPasswordOTP -resetPasswordOTPExpires"
-    )
-    .populate({
-      path: "profile",
-      select: "-__v -createdAt -updatedAt",
-    });
+  const user = await User.findById(req.user._id).populate({
+    path: "profile",
+    select: "-__v -createdAt -updatedAt",
+  });
 
   res.status(200).json({
     status: "success",
